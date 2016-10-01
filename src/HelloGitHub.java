6311a7b459385ee83ad667fd075125e24cc58fc4
Viktoria Schlaipfer <viktoria.schlaipfer@edu.campus02.at>
2016-10-01 11:14:48 +0200

    das ist ein weiterer Test

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -2,7 +2,8 @@
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		System.out.println("Hello GitHub, I'm developer 2!, test");
+		System.out.println("Hello GitHub, I'm developer 2");
+		System.out.println("Hello GitHub, I'm developer 2.1");
 
 	}
 
